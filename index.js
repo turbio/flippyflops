d9b44d1270321cbd35ef4f410c36282678a78132
Mason Clayton <turbio@users.noreply.github.com>
2021-07-10 04:32:24 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,269 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const Database = require("@replit/database")
+
+const db = new Database()
+
+const app = express();
+app.use(bodyParser.text({type: '*/*'}));
+app.use(express.static('public'));
+
+let listen = [];
+const send = cmd => {
+  listen.forEach(l => l(cmd));
+}
+
+let last_data = null;
+const hold_time = 1000*60*1;
+
+const msgd = () => last_data = new Date();
+
+app.all('/msg/:msg', (req, res) => {
+  msgd();
+  const body = req.params.msg;
+  if (!body) return res.end('uwu daddy');
+  console.log('msg', body)
+  db.set('msg:'+(+new Date()), body);
+  send([
+    {c:0},
+    {s: body.slice(0,14), x: 1, y: 1},
+    {s: body.slice(14), x: 1, y: 7},
+    {d:1},
+  ]);
+  res.send();
+});
+
+app.get('/msg', (req, res) => {
+  const body = req.query.msg;
+  if (!body) return res.redirect('/');
+
+  msgd();
+  console.log('msg', body)
+  db.set('msg:'+(+new Date()), body);
+
+  send([
+    {c:0},
+    {s: body.slice(0,14), x: 1, y: 1},
+    {s: body.slice(14), x: 1, y: 7},
+    {d:1},
+  ]);
+  res.redirect('/');
+});
+
+app.post('/msg', (req, res) => {
+  const body = req.body;
+  if (!body) return res.redirect('/');
+
+  msgd();
+  console.log('msg', body)
+  db.set('msg:'+(+new Date()), body);
+
+  send([
+    {c:0},
+    {s: body.slice(0,14), x: 1, y: 1},
+    {s: body.slice(14), x: 1, y: 7},
+    {d:1},
+  ]);
+  res.redirect('/');
+});
+
+app.post('/set', (req, res) => {
+  msgd();
+  let bod;
+	try {
+		bod = JSON.parse(req.body);
+	}
+	catch(error) {
+		console.log(error);
+		res.status(400).send('Not Valid JSON!');
+    return;
+	}
+
+  let bmp = [...new Array(56*14)].fill('.');
+  bod.pixels
+    .forEach(({x,y,state}) => {bmp[x+(y*56)] = state ? '#' : ' '});
+  console.log('|'+'='.repeat(56-2)+'|');
+  console.log(bmp.join('').match(/.{1,56}/g).join("\n"));
+  console.log('|'+'='.repeat(56-2)+'|');
+  db.set('img:'+(+new Date()), bmp.join(''));
+
+	if(bod.pixels.length <= 0) return;
+	let sendQueue = [];
+	for(var i = 0; i < bod.pixels.length; i++) {
+		const pix = bod.pixels[i];
+		if(typeof pix.x != 'number' || typeof pix.y != 'number') res.status(400).send('TypeError: pixel location is not a number');
+		if(typeof pix.state != 'boolean') res.status(400).send('TypeError: State is not a boolean');
+		var state = pix.state ? 1 : 0;
+		sendQueue.push({
+			p: state,
+			x: pix.x,
+			y: pix.y,
+		});
+	}
+	sendQueue.push({d:1});
+	send(sendQueue);
+	res.status(201).send('DRAWN');
+});
+
+app.all('/set/:x/:y/:on', (req, res) => {
+  msgd();
+  console.log(req.params)
+  send([
+    {
+      p: (req.params.on=='0'||req.params.on=='false') ? 0 : 1,
+      x: parseInt(req.params.x),
+      y: parseInt(req.params.y),
+    },
+    {d:1},
+  ]);
+  res.send();
+});
+
+app.get('/stream', (req, res) => {
+  console.log('incoming stream!')
+  const cb = cmd => {
+    cmd.forEach(c => {
+      res.write(JSON.stringify(c)+'\n');
+    })
+  }
+
+  listen.push(cb);
+
+  req.on("close", () => {
+    listen = listen.filter(l => l !== cb);
+  });
+});
+
+const stats_width = 56;
+const hist = [...new Array(stats_width)].fill(undefined);
+let last_stat = null;
+let stat_name = '';
+let stat_interv = 0;
+
+
+
+app.post('/statclear', (req, res) => {
+  const b = JSON.parse(req.body)
+
+  stat_name = b.name;
+  stat_interv = b.interv;
+  last_stat = null;
+  hist.fill(undefined);
+  res.end();
+});
+
+app.post('/stat', (req, res) => {
+  msgd();
+
+  if (!req.body.match(/^[0-9.]+$/)) {
+    return;
+  }
+
+  const v = parseFloat(req.body)
+
+  if (hist[hist.length-1]) {
+    if (v > hist[hist.length-1].hi) {
+      hist[hist.length-1].hi = v;
+    }
+
+    if (v < hist[hist.length-1].lo) {
+      hist[hist.length-1].lo = v;
+    }
+  } else {
+    hist[hist.length-1] = {hi: v, lo: v, open: v};
+  }
+
+  if (!last_stat || new Date() - last_stat > stat_interv) {
+    hist.push({hi: v, lo: v, open: v});
+    hist.shift();
+    last_stat = new Date();
+  }
+
+  let hi = Number.MIN_SAFE_INTEGER;
+  let lo = Number.MAX_SAFE_INTEGER;
+  hist.filter(l => !!l).forEach(l => {
+    hi = Math.max(hi, l.hi);
+    lo = Math.min(lo, l.lo);
+  });
+
+  const mapl = v => {
+    const disp_hi = 5;
+    const disp_lo = 13;
+
+    return Math.floor(((v - lo) / (hi - lo)) * (disp_hi - disp_lo)) + disp_lo
+  }
+
+  const cmd = [
+    {c: 0}, {s: stat_name+' '+v, x:1, y:0}
+  ];
+  hist.forEach((l, i) => {
+    if (!l) return;
+
+    let fr, to;
+    if (i == hist.length-1) {
+      fr = mapl(l.open)
+      to = mapl(v);
+    } else {
+      fr = mapl(l.hi);
+      to = mapl(l.lo);
+    }
+
+    for (let y = Math.min(fr, to); y <= Math.max(fr, to); y++) {
+      cmd.push({p: 1, x: i, y})
+    }
+  });
+  cmd.push({d:1})
+  send(cmd);
+
+  res.end();
+});
+
+setInterval(() => {
+  if (new Date() - last_data < hold_time) return;
+
+  const now = new Date(new Date().toLocaleString("en-US", { timeZone: "America/Los_Angeles" }));
+
+  const num = [
+    "one",   "two",   "three", "four", "five",   "six",
+    "seven", "eight", "nine",  "ten",  "eleven", "twelve",
+  ];
+
+  const angle = [
+    undefined,       "five",    "ten", "quarter", "twenty", "twenty five",
+    "half",   "twenty five", "twenty", "querter",    "ten", "five",
+  ]
+
+  let a;
+  let b;
+
+  if (now.getMinutes() < 5) {
+    a = num[(now.getHours() - 1 + 12) % 12];
+    b = "o'clock";
+  } else if (now.getMinutes() < 35) {
+    a = angle[Math.floor(now.getMinutes() / 5)];
+    b = `past ${num[(now.getHours() - 1 + 12) % 12]}`;
+  } else {
+    a = angle[Math.floor(now.getMinutes() / 5)];
+    b = `to ${num[now.getHours() % 12]}`;
+  }
+  
+  send([
+    {c: 0},
+    {s: a, x: 2, y: 1},
+    {s: b, x: 2, y: 8},
+    {
+      p: 1,
+      x: 54 + Math.floor((now.getSeconds() % 4)/2),
+      y: Math.floor(((now.getSeconds()+1) % 4)/2),
+    },
+    {d:1},
+  ]);
+}, 1000)
+
+setInterval(() => {
+  send([{}]);
+}, 2000)
+
+app.listen(3000, () => {
+  console.log('suwuver stuwurted UwU');
+});
---
+++ public/editor.js
@@ -0,0 +1,309 @@
+const BLACK = '#000000';
+const WHITE = '#FFFFFF';
+const WIDTH = 56;
+const HEIGHT = 14;
+const scale = 10
+
+class Picture {
+  constructor(width, height, pixels) {
+    this.width = width
+    this.height = height
+    this.pixels = pixels
+  }
+
+  static empty(width, height, color) {
+    let pixels = new Array(width * height).fill(color)
+    return new Picture(width, height, pixels)
+  }
+ 
+  pixel(x, y) {
+    return this.pixels[x + y * this.width]
+  }
+
+  draw(pixels) {
+    let copy = this.pixels.slice()
+    for (let { x, y, color } of pixels) {
+      copy[x + y * this.width] = color
+    }
+    return new Picture(this.width, this.height, copy)
+  }
+}
+
+function elt(type, props, className, ...children) {
+  let dom = document.createElement(type)
+  if (props) Object.assign(dom, props)
+  for (let child of children) {
+    if (typeof child != 'string') dom.appendChild(child)
+    else dom.appendChild(document.createTextNode(child))
+  }
+  if (className) dom.classList.add(className);
+  return dom
+}
+
+class PictureCanvas {
+  constructor(picture, pointerDown) {
+    this.dom = elt('canvas', {
+      onmousedown: event => this.mouse(event, pointerDown),
+      ontouchstart: event => this.touch(event, pointerDown)
+    })
+    this.syncState(picture)
+  }
+  syncState(picture) {
+    if (this.picture == picture) return
+    drawPicture(picture, this.dom, scale, this.picture)
+    this.picture = picture
+  }
+}
+
+function drawPicture(picture, canvas, scale, previous) {
+  if (previous == null ||
+    previous.width != picture.width ||
+    previous.height != picture.height) {
+    canvas.width = picture.width * scale
+    canvas.height = picture.height * scale
+    previous = null
+  }
+
+  let cx = canvas.getContext('2d')
+  for (let y = 0; y < picture.height; y++) {
+    for (let x = 0; x < picture.width; x++) {
+      let color = picture.pixel(x, y)
+      if (previous == null || previous.pixel(x, y) != color) {
+        cx.fillStyle = color
+        cx.fillRect(x * scale, y * scale, scale, scale)
+      }
+    }
+  }
+}
+
+PictureCanvas.prototype.mouse = function(downEvent, onDown) {
+  if (downEvent.button != 0) return
+  let pos = pointerPosition(downEvent, this.dom)
+  let onMove = onDown(pos)
+  if (!onMove) return
+  let move = moveEvent => {
+    if (moveEvent.buttons == 0) {
+      this.dom.removeEventListener('mousemove', move)
+    } else {
+      let newPos = pointerPosition(moveEvent, this.dom)
+      if (newPos.x == pos.x && newPos.y == pos.y) return
+      pos = newPos
+      onMove(newPos)
+    }
+  }
+  this.dom.addEventListener('mousemove', move)
+}
+
+function pointerPosition(pos, domNode) {
+  let rect = domNode.getBoundingClientRect()
+  return {
+    x: Math.min(Math.floor((pos.clientX - rect.left) / rect.width * WIDTH), WIDTH-1),
+    y: Math.min(Math.floor((pos.clientY - rect.top) / rect.height * HEIGHT), HEIGHT-1)
+  }
+}
+
+PictureCanvas.prototype.touch = function(startEvent, onDown) {
+  let pos = pointerPosition(startEvent.touches[0], this.dom)
+  let onMove = onDown(pos)
+  startEvent.preventDefault()
+  if (!onMove) return
+  let move = moveEvent => {
+    let newPos = pointerPosition(moveEvent.touches[0],
+      this.dom)
+    if (newPos.x == pos.x && newPos.y == pos.y) return
+    pos = newPos
+    onMove(newPos)
+  }
+  let end = () => {
+    this.dom.removeEventListener('touchmove', move)
+    this.dom.removeEventListener('touchend', end)
+  }
+  this.dom.addEventListener('touchmove', move)
+  this.dom.addEventListener('touchend', end)
+}
+
+class PixelEditor {
+  constructor(state, config) {
+    let { controls, dispatch } = config
+    this.state = state
+
+    this.canvas = new PictureCanvas(state.picture, pos => {
+      let onMove = draw(pos, this.state, dispatch)
+      if (onMove) return pos => onMove(pos, this.state)
+    })
+    this.controls = controls.map(
+      Control => new Control(state, config))
+    this.dom = elt(
+      'div',
+      { tabIndex: 0, onkeydown: event => this.keyDown(event, config) }, '',
+      elt('div', {}, 'canvc', this.canvas.dom),
+      ...this.controls.reduce((a, c) => a.concat(' ', c.dom), []))
+  }
+
+  keyDown(event, config) {
+    if (event.key == 'z' && (event.ctrlKey || event.metaKey)) {
+      event.preventDefault()
+      config.dispatch({ undo: true })
+    }
+  }
+
+  syncState(state) {
+    this.state = state
+    this.canvas.syncState(state.picture)
+    for (let ctrl of this.controls) ctrl.syncState(state)
+  }
+}
+
+function drawLine(from, to, color) {
+  let points = []
+  if (Math.abs(from.x - to.x) > Math.abs(from.y - to.y)) {
+    if (from.x > to.x) [from, to] = [to, from]
+    let slope = (to.y - from.y) / (to.x - from.x)
+    for (let { x, y } = from; x <= to.x; x++) {
+      points.push({ x, y: Math.round(y), color })
+      y += slope
+    }
+  } else {
+    if (from.y > to.y) [from, to] = [to, from]
+    let slope = (to.x - from.x) / (to.y - from.y)
+    for (let { x, y } = from; y <= to.y; y++) {
+      points.push({ x: Math.round(x), y, color })
+      x += slope
+    }
+  }
+  return points
+}
+
+function draw(pos, state, dispatch) {
+  if (state.picture.pixel(pos.x, pos.y) == '#FFFFFF') {
+    dispatch({ color: state.color = '#000000' })
+  } else {
+    dispatch({ color: state.color = '#FFFFFF' })
+  }
+
+  function connect(newPos, state) {
+    let line = drawLine(pos, newPos, state.color)
+    pos = newPos
+    dispatch({ picture: state.picture.draw(line) })
+  }
+  connect(pos, state)
+
+  return connect
+}
+
+class SendButton {
+  constructor(state) {
+    this.picture = state.picture
+    this.dom = elt('button', {
+      'class': 'sendbutton',
+      onclick: () => this.save()
+    }, 'sendbutton', 'Send')
+  }
+
+  save() {
+    this.dom.classList.add('sending');
+    postPx(picToPx(this.picture)).then(()=>{
+        this.dom.classList.remove('sending');
+        document.getElementById('thnx2').style.display = 'block';
+      })
+  }
+
+  syncState(state) { this.picture = state.picture }
+}
+
+class ClearButton {
+  constructor(state, { dispatch }) {
+    this.picture = state.picture
+    this.dom = elt('button', {
+      onclick: () => this.fill(dispatch)
+    }, 'clearbutton', 'Clear')
+  }
+
+  fill(dispatch) {
+    const allBlack = this.picture.pixels.every(e => e === '#000000')
+
+    const drawn = []
+    for (let x = 0; x < 56; x++) {
+      for (let y = 0; y < 14; y++) {
+        drawn.push({ x, y, color: allBlack ? '#FFFFFF' : '#000000' });
+      }
+    }
+    dispatch({ picture: this.picture.draw(drawn) })
+  }
+  syncState(state) { this.picture = state.picture }
+}
+
+function historyUpdateState(state, action) {
+  if (action.undo) {
+    if (state.done.length == 0) return state
+    return Object.assign({}, state, {
+      picture: state.done[0],
+      done: state.done.slice(1),
+      doneAt: 0
+    })
+  } else if (action.picture && state.doneAt < Date.now() - 1000) {
+    return Object.assign({}, state, action, {
+      done: [state.picture, ...state.done],
+      doneAt: Date.now()
+    })
+  } else {
+    return Object.assign({}, state, action)
+  }
+}
+
+class UndoButton {
+  constructor(state, { dispatch }) {
+    this.dom = elt('button', {
+      onclick: () => dispatch({ undo: true }),
+      disabled: state.done.length == 0
+    }, 'undobutton', 'Undo')
+  }
+  syncState(state) {
+    this.dom.disabled = state.done.length == 0
+  }
+}
+
+function startPixelEditor() {
+    let state = {
+      color: WHITE,
+      picture: Picture.empty(56, 14, BLACK),
+      done: [],
+      doneAt: 0
+    };
+
+    controls = [ClearButton, UndoButton, SendButton];
+
+  const app = new PixelEditor(state, {
+    controls,
+    dispatch(action) {
+      state = historyUpdateState(state, action)
+      app.syncState(state)
+    }
+  })
+
+  return app.dom
+}
+
+document.querySelector('#editor')
+  .appendChild(startPixelEditor())
+
+const picToPx = (picture) => {
+  let pixels = [];
+  for (let i = 0; i < 56; i++) {
+    for (let j = 0; j < 14; j++) {
+      pixels.push({
+        x: i,
+        y: j,
+        state: picture.pixel(i, j) == '#FFFFFF',
+      })
+    }
+  }
+  return pixels;
+}
+
+const postPx = pixels =>  fetch('https://dots.turb.io/set', {
+    method: 'POST',
+    body: JSON.stringify({
+      pixels: pixels,
+    }),
+  });
